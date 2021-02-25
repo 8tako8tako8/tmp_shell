348851a3c15dffbc329471286c082c4dcc1cfe6c
森本晃輔 <8tako8tako8@MacBook-Pro.local>
2021-02-25 18:49:09 +0900

    add: ft_pwdを追加
    ft_pwdを追加、データ長は100とした

---
+++ pwd/pwd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void    ft_pwd(void)
+{
+    char    path[100];
+    int     i;
+
+    i = 0;
+    while (i < 100)
+    {
+        path[i] = 0;
+        i++;
+    }
+    if (!(getcwd(path, 100)))
+        exit(1);
+    printf("%s\n", path);
+}
+
+int main()
+{
+    ft_pwd();
+}
