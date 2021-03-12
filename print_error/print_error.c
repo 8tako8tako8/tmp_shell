9dd3b53c0b154d2e512a491a13091b1d78f670a2
森本晃輔 <8tako8tako8@MacBook-Pro.local>
2021-03-12 17:54:17 +0900

    add: malloc失敗用のprint_error_and_exitを追加

--- print_error/print_error.c
+++ print_error/print_error.c
@@ -1,18 +1,26 @@
+#include <string.h>
+#include <errno.h>
 #include "../libft/libft.h"
 
-void	print_error(char *cmd, char *args, char *error_msg)
+void	print_error(char *cmd, char *arg, char *error_msg)
 {
 	ft_putstr_fd("minishell: ", 2);
 	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd(": ", 2);
-	if (args)
+	if (arg)
 	{
 		ft_putstr_fd("`", 2);
-		ft_putstr_fd(args, 2);
+		ft_putstr_fd(arg, 2);
 		ft_putstr_fd("'", 2);
 		ft_putstr_fd(": ", 2);
 	}
 	ft_putendl_fd(error_msg, 2);
+}
+
+void	print_error_and_exit(void)
+{
+	ft_putendl_fd(strerror(errno), 2);
+	exit(1);
 }
 
 int		main()
